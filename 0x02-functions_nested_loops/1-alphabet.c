bbb7cdcd6379e40ca5a27f3f33dd89cce1e06140
Ezekielcoolb <ezekielcoolb@gmail.com>
2022-12-14 20:58:46 -0800

    function c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabelt - print alphabet in lowercase
+ */
+
+void print_alphabet(void);
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putcahr('\n')
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+project on function for c language
